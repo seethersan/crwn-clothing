009044724ae75036409ac4c0db4180cf9cf626ed
seethersan <carlos_jcez@hotmail.com>
2021-12-16 20:19:21 -0500

    update menu-items to navigate withRouter

---
+++ src/component/directory.component/directory.component.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import './directory.component.scss'
+import sections from "./directory.data";
+import MenuItem from "../menu-item/menu-item.component";
+
+class Directory extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            sections: sections
+        }
+    }
+
+    render() {
+        return (
+            <div className="directory-menu">
+                {
+                    this.state.sections.map(({id, ...otherSectionProps}) => (
+                        <MenuItem key={id} {...otherSectionProps}/>
+                    ))
+                }
+            </div>
+        )
+    }
+}
+
+export default Directory;
